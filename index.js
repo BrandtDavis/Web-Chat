795952f7334cb8af9665fd05aad03609fdc07fc8
CS\brandt.davis <brandt.davis@ucalgary.ca>
2020-03-06 17:49:42 -0700

    restyled page, added some nickname support

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
+
+/*     SENG 513     */
+/*   Assignment 3   */
+/********************/
+/*   Brandt Davis   */
+/*     30019817     */
+/* Lab Section B02  */
+/********************/
+
 var app = require('express')();
+var path = require('path');
 var http = require('http').createServer(app);
+
 var io = require('socket.io')(http);
 
-// This function takes a date object and 
+var server = require('http').Server(app);
+
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
+// I used information in this link to serve my CSS file so I could separate code from the html file
+// ---- https://stackoverflow.com/questions/44680410/serving-static-javascript-files-with-node-js-socket-io-and-express
+   var express = require('express');
+   app.use(express.static(path.join(__dirname, 'public')));
+// --------------------------------------------------------------------------------------------------------------------
+
+// Store possible nicknames for users in a global array
+// along with a count of how many users are online
+var nickNames = ["nick_cage", "ESPDude55", "datBoi", "Pepe", "Frogger"];
+var numUsers = 0;
+
+var activeUsers = {};
+
+
+// This function takes a date objecnullt and 
 // outputs the textual version of month 
 // since getMonth() only returns a value of 0-11
 // ---------------------------------------------
@@ -26,18 +56,14 @@ function calcMonth(date){
   return monthDict[monthNum];
 }
 
+
 // This function takes a date object and 
 // outputs a nicely formatted string of hours, mintues, etc.
-function calcTime(date){
+function calcTime(date){for (var socketid in io.sockets.sockets) {}
   let hour = date.getHours();
-  let min = date.getMinutes();
+  let min = date.getMinutes(); 
   let time = "";
 
-  // Minutes under 10 only appear as a single digit
-  // Check if it is single digit, and if so, prepend "0"
-  if(min % 10 === min){
-    min = "0" + String(min); 
-  }
   // Check if hour is greater than 12, so we can convert
   // from 24 hr to 12 hr clock and add "pm"
   if(hour > 12) {
@@ -50,29 +76,41 @@ function calcTime(date){
   return time;
 }
 
+var nickName = "yeye";
+
 app.get('/', function(req, res){
+  //res.send(req.cookies);
+  console.log('Cookies: ', req.cookies);
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+    
     console.log('a user connected');
-    socket.on('chat message', function(msg, date){
-        let d = new Date();
-        let month = calcMonth(d);
-        let day = d.getDate();
-        let year = d.getFullYear()
-        let time = calcTime(d);
+    activeUsers[socket.id] = nickNames[numUsers];
+    console.log(activeUsers[socket.id]);
+    numUsers++;
 
-        date = [month, day, year, time];
+    app.get('/', function(req, res){
+      res.cookie("nickName2", nickName);
+    });
 
-        //console.log(date[0]);
-        //console.log(date[1]);
-        //console.log(date[2]);
-        //console.log(date[3]);
-        io.emit('chat message', msg, date);
+    socket.on('chat message', function(msg, date, nickName){
+        let d = new Date();         // Instantiate new Date object
+        let month = calcMonth(d);   // Call calcMonth to get the string form of the current month
+        let day = d.getDate();      // Get the current day of the month
+        let year = d.getFullYear(); // Get the current year
+        let time = calcTime(d);     // Get the current hour and minutes
+
+        date = [month, day, year, time];  // Store date information in an array to emit to client
+
+        nickName = activeUsers[socket.id];  
+
+        io.emit('chat message', msg, date, nickName);
       });
 
     socket.on('disconnect', function(){
+      numUsers--;
       console.log('user disconnected');
     });
   });
